87e86a2541f74176ceab68cec17b9bb1f49e3436
Kamran Musadirli <musadirlikamran@gmail.com>
2026-05-26 09:31:33 +0200

    Add hero + stats section (with stub info)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./components/common/Header/Header";
 
+import Hero from "./components/features/Hero/Hero";
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <Hero />
     </div>
   );
 }
