f6c2db2cb1410137800296bddc75aea9d529f16f
Kamran Musadirli <musadirlikamran@gmail.com>
2026-05-26 13:13:48 +0200

    Resolve merge conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-
-
-
-import Contact from './pages/Contact/Contact'
 import "./App.css";
 import Header from "./components/common/Header/Header";
-import FeaturedProducts from "./components/features/FeaturedProducts/FeaturedProducts";
-=======
-import "./App.css";
-import Header from "./components/common/Header/Header";
->>>>>>> origin/master
 import Hero from "./components/features/Hero/Hero";
 import FeaturedProducts from "./components/features/FeaturedProducts/FeaturedProducts";
 
@@ -18,13 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Hero />
-<<<<<<< HEAD
-
-
-=======
->>>>>>> origin/master
       <FeaturedProducts />
-
     </div>
   );
 }
