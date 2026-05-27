7802cbcc9bf1777bef24a057831e8d4d06c888ca
Jaroslav Nesen <jara.nesen@gmail.com>
2026-05-27 13:51:17 +0200

    Add improved transitions between pages, clickable logo to redirect to home page

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,14 +1,17 @@
 import Logo from "../Logo/Logo";
 import Navigation from "../Header/Navigation/Navigation";
 import "./Footer.css";
+import { Link } from "react-router";
 
 const Footer = () => {
   return (
     <div className="footer">
       <footer>
-        <div className="logo">
-          <Logo />
-        </div>
+        <Link to={"/"}>
+          <div className="logo">
+            <Logo />
+          </div>
+        </Link>
         <div className="navigation">
           <Navigation />
         </div>
